3cc0d4ccf83784ad590aa0e2021e0490bdea12de
pumamaheswaran <pumamaheswaran@users.noreply.github.com>
2015-04-08 23:56:49 -0400

    Problem 1 and Problem 5 changes

--- ML-PA2/script.py
+++ ML-PA2/script.py
@@ -3,7 +3,7 @@ University at Buffalo - Spring 2015
 CSE 574 - Introduction to Machine Learning
 Programming Assignment 2
 
-Authors: Dheeraj Balakavi, Pravin Umamaheshwaran, Mithun
+Authors: Dheeraj Balakavi, Pravin Umamaheswaran, Mithun
 
 '''
 import numpy as np
@@ -25,6 +25,38 @@ def ldaLearn(X,y):
     
     # IMPLEMENT THIS METHOD
     
+    
+    means = np.array([])
+    unique_y = np.unique(y)
+    
+    i = 0;
+    # processing each unique values in batches
+    for index_val in unique_y:
+        
+        mapp_matrix = (y == index_val)
+        mapp_matrix.shape = ((1,X.shape[0])) 
+        #extended matrix
+        ex_mapp_matrix = np.tile(mapp_matrix,X.shape[1])
+        ex_mapp_matrix = np.reshape(ex_mapp_matrix,(X.shape[1],X.shape[0]))
+        ex_mapp_matrix = ex_mapp_matrix.T
+       
+        MeanMat = np.asarray(X[ex_mapp_matrix])
+        new_row = MeanMat.shape[0]/X.shape[1]
+       
+        KIK = np.reshape(MeanMat,(new_row,X.shape[1]))
+        #taking the maen values of i/p per input data variable(x^i) per class
+        Mean_Val = KIK.mean(0)
+   
+        if i == 0:
+            means = np.hstack(Mean_Val)
+        else :
+            means = np.vstack((means,Mean_Val))  
+
+        i+=1  
+          
+    means = means . T
+    covmat = np.cov(X.T) # i am not sure about the covarience
+        
     return means,covmat
 
 def qdaLearn(X,y):
@@ -37,7 +69,42 @@ def qdaLearn(X,y):
     # covmats - A list of k d x d learnt covariance matrices for each of the k classes
     
     # IMPLEMENT THIS METHOD
+    covmats = list()
+    unique_classes = np.unique(y)
+    mean_vectors = []
+    for cl in range(1,5):
+        mean_vectors.append(np.mean(X[y==cl], axis=0))
     
+    i =0 
+    for class_val in unique_classes:
+         mapping_matrix = (y==class_val)
+         mapping_matrix.shape = ((1,X.shape[0]))
+         
+         # creating an extended matrix by using tile function
+         # to match the dimensions and separate by class
+            
+         ext_map_matrix = np.tile(mapping_matrix,X.shape[1])
+         ext_map_matrix = np.reshape(ext_map_matrix,(X.shape[1],X.shape[0]))
+         ext_map_matrix = ext_map_matrix.T
+         
+         class_ip = np.asarray(X[ext_map_matrix])
+         row_size = class_ip.shape[0]/X.shape[1]
+         
+         class_ip = np.reshape(class_ip,(row_size,X.shape[1]))
+         covmat = np.cov(class_ip.T)
+         covmats.append(covmat)
+       
+         #take mean values of i/p per input data variable per class
+         mean_value = class_ip.mean(0)
+         if i == 0:
+            means = np.hstack(mean_value)
+         else :
+            means = np.vstack((means,mean_value))  
+
+         i+=1  
+          
+    means = means . T
+  
     return means,covmats
 
 def ldaTest(means,covmat,Xtest,ytest):
@@ -49,6 +116,30 @@ def ldaTest(means,covmat,Xtest,ytest):
     # acc - A scalar accuracy value
     
     # IMPLEMENT THIS METHOD
+    no_of_classes = 5
+    inv_covariance = np.linalg.inv(covmat)  
+    N = Xtest.shape[0]
+   
+    calc_label = np.array([])
+    #calc_label = np.reshape(calc_label,(100,1))
+    new_mean = means.T
+    i = 0
+    for each_row in range(N):   # for each row data calculate the probability or the classes
+         row_data = Xtest[each_row,:]
+         pdf_vector = np.array([])
+         for each_class in range(no_of_classes):
+             X_MU = row_data - new_mean[each_class]
+             intmdt = np.dot(X_MU.T,inv_covariance)
+             temp    = np.dot(intmdt,X_MU)
+             d = 1
+             pdf = np.exp(-1/2*temp)
+             pdf_vector = np.append(pdf_vector,pdf)
+             #pdf_vector[each_class] = pdf
+         calc_label = np.append(calc_label,[np.argmax(pdf_vector)+1]) 
+    calc_label  = np.matrix(calc_label).T      
+   
+    acc = 100 * np.mean((calc_label == ytest).astype(float)) 
+    #plotGraph(Xtest,ytest,calc_label)
     return acc
 
 def qdaTest(means,covmats,Xtest,ytest):
@@ -60,6 +151,30 @@ def qdaTest(means,covmats,Xtest,ytest):
     # acc - A scalar accuracy value
     
     # IMPLEMENT THIS METHOD
+    #no_of_classes = 5
+    k = 5
+    N = Xtest.shape[0]
+    i= 0
+    new_mean = means.T
+    calc_label = np.array([])
+    for each_row in range(N):  # for each row of the data calculate the probability 
+      x_i = Xtest[each_row,:]
+      pdf_vector = np.array([])
+      for cl,cov_mat in zip(range(1,6),covmats):
+          inv_covmat = np.linalg.inv(cov_mat) # inverse covariance matrix
+          normlz_factor = 1/np.sqrt((2*np.pi)**k * np.linalg.det(cov_mat))
+         
+          X_MU   = x_i - new_mean[cl-1]
+          intmdt = np.dot(X_MU.T,inv_covmat)
+          temp   = np.dot(intmdt,X_MU)
+          pdf    = np.exp(-1/2*temp)*normlz_factor
+          pdf_vector = np.append(pdf_vector,pdf)
+      calc_label = np.append(calc_label,[np.argmax(pdf_vector)+1])
+    
+    calc_label = np.matrix(calc_label).T      
+    
+    acc = 100 * np.mean((calc_label == ytest).astype(float)) 
+    
     return acc
 
 # Author: Dheeraj Balakavi
@@ -166,6 +281,7 @@ def regressionObjVal(w, X, y, lambd):
     error_grad = np.squeeze(np.asarray(error_grad))                            
     return error, error_grad
 
+
 def mapNonLinear(x,p):
     # Inputs:                                                                  
     # x - a single column vector (N x 1)                                       
@@ -173,14 +289,29 @@ def mapNonLinear(x,p):
     # Outputs:                                                                 
     # Xd - (N x (d+1))                                                         
     # IMPLEMENT THIS METHOD
-    Xd = ""
+    output = []
+    for val in np.nditer(x):
+        
+        #if p == 0:
+        #    Xd = np.ones((x.shape[0],1))
+        #    break
+        
+        for power  in range(0,p+1):
+            #print 'awesome'            
+            output.append(pow(val,power))
+
+    #print 'x:' + str(x.shape[0])
+    #print 'p:' + str(p)
+    #print 'Size is:' + str(len(output))
+    Xd = np.asarray(output)
+    Xd = np.reshape(Xd,(x.shape[0],p+1))
     return Xd
 
 # Main script
 
 # Problem 1
 # load the sample data 
-'''                                                                
+                                                               
 X,y,Xtest,ytest = pickle.load(open('sample.pickle','rb'))            
 
 # LDA
@@ -191,7 +322,7 @@ print('LDA Accuracy = '+str(ldaacc))
 means,covmats = qdaLearn(X,y)
 qdaacc = qdaTest(means,covmats,Xtest,ytest)
 print('QDA Accuracy = '+str(qdaacc))
-'''
+
 # Problem 2
 X,y,Xtest,ytest = pickle.load(open('diabetes.pickle','rb'))   
 # add intercept
@@ -237,7 +368,7 @@ for lambd in lambdas:
 plt.plot(lambdas,rmses4)
 
 # Problem 5
-''' 
+ 
 pmax = 7
 lambda_opt = lambdas[np.argmin(rmses4)]
 rmses5 = np.zeros((pmax,2))
@@ -246,8 +377,8 @@ for p in range(pmax):
     Xdtest = mapNonLinear(Xtest[:,2],p)
     w_d1 = learnRidgeRegression(Xd,y,0)
     rmses5[p,0] = testOLERegression(w_d1,Xdtest,ytest)
-    w_d2 = learnRidgeRegression(Xd,y,lamda_opt)
+    w_d2 = learnRidgeRegression(Xd,y,lambda_opt)
     rmses5[p,1] = testOLERegression(w_d2,Xdtest,ytest)
 plt.plot(range(pmax),rmses5)
 plt.legend(('No Regularization','Regularization'))
-''' 
+ 
